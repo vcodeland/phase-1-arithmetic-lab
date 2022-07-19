c7d093d2f21fd16e56e1062cd490c0356bf16214
Vernitra Jones <vernitrasmedia@gmail.com>
2022-07-18 21:01:16 -0500

    commit tree f6ea92dd123f07294649d75fdf2f363799f92dab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random()) + 1
+
+const num3 = 9;
+const num4 = 5;
+const mod = num3 % num4;
+
+let max;
+max = Math.max(3, 8, 20);
